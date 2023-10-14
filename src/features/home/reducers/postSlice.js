646dffb844b7a789ced52a1c87b9154d7c2cfb2a
Irawo <timothyaremu63@gmail.com>
2023-10-14 11:56:33 +0100

    create postSlice in home

---
+++ src/features/home/reducers/postSlice.js
@@ -0,0 +1,30 @@
+import {
+  createSlice,
+  createEntityAdapter,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+
+const initialState = createEntityAdapter({
+  status: "pending",
+  error: null,
+  count: 0,
+});
+
+const postSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        updateCount: {
+            reducer: (state, action) => {
+                state.count = state.count + action.payload + 1
+            },
+            prepare(num){
+                return {
+                    payload: {
+                        num
+                    }
+                }
+            }
+        }
+    }
+})
