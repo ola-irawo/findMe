14c39dc5f60dc6f118f0a5ed31f8c6d18d15fe6b
Irawo <timothyaremu63@gmail.com>
2023-10-29 01:46:18 +0100

    add base name to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="findMe">
         <App />
       </BrowserRouter>
     </Provider>
